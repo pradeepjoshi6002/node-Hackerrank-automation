7bb9e527958f1aa0476173d7c16ddc369a8d9d43
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2024-01-14 20:10:07 +0530

    second commit

---
+++ one.js
@@ -0,0 +1,34 @@
+import puppeteer from "puppeteer";
+let page;
+puppeteer
+  .launch({ headless: false })
+  .then((browser) => {
+    const pagesArrPomise = browser.pages();
+    return pagesArrPomise;
+  })
+  .then((pagesArrPomise) => {
+    page = pagesArrPomise[0];
+    let gotoPromise = page.goto("https://github.com/login");
+    return gotoPromise;
+  })
+  .then(function () {
+    let elementWaitPromise = page.waitForSelector("input[type='text']", {
+      visible: true,
+    });
+    return elementWaitPromise;
+  })
+  .then(function () {
+    console.log("reached google home page.");
+    let keyswillsendPromises = page.type(
+      "input[type='text']",
+      "pradeepjoshi6002@gmail.com"
+    );
+    return keyswillsendPromises;
+  })
+  .then(function () {
+    let enterwillbepressed = page.keyboard.press("Enter");
+    return enterwillbepressed;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
